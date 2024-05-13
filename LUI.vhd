1ee06307b44a18aa86193f61baaf92b2f7ff12be
pedrocivita <pedro.civita@gmail.com>
2024-05-13 18:36:47 -0300

    LUIs

---
+++ LUI.vhd
@@ -0,0 +1,26 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- Declaração da entidade LUI para carregar os 16 bits superiores de um registrador de 32 bits com valor imediato.
+
+entity LUI is
+    port
+    (
+        input  : in  std_logic_vector(15 downto 0);  -- Entrada de 16 bits.
+        output : out std_logic_vector(31 downto 0)   -- Saída de 32 bits, com 16 bits inferiores zerados.
+    );
+	 
+end entity;
+
+-- Arquitetura comportamental do componente LUI.
+
+architecture comportamento of LUI is
+
+begin
+
+    -- Concatena a entrada de 16 bits com 16 bits de zeros para formar a saída de 32 bits.
+    -- Esta operação carrega a parte imediata no registro com zeros nos bits inferiores.
+	 
+    output <= input & "0000000000000000";  -- Adiciona 16 zeros diretamente à entrada para formar a saída.
+	 
+end architecture;
